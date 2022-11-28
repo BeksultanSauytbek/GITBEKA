f61b2e99c1bef330c21804eba6b939ea742abeb5
BeksultanSauytbek <220103118@stu.sdu.edu.kz>
2022-11-28 20:39:07 +0600

    tillsaturday

---
+++ src/Questions.java
@@ -0,0 +1,11 @@
+public class Questions {
+    public static void main(String[] args) {
+        System.out.println("Name: Beksultan");
+        System.out.println("Surname: Sauytbek");
+        System.out.println("Id: 220103118");
+        System.out.println("Hobby: CSGO");
+        System.out.println("How much do you like ICT(from 1-10)): 1000");
+        System.out.println("Rate this task from 1 to 10: 100");
+        System.out.println("Am I the best teacher?: ES");
+    }
+}
